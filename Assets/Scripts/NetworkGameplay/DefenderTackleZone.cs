89349b34451a88bd59f330af38b70169d1d614f8
RomeWithLove <138332444+RomeWithLove@users.noreply.github.com>
2026-09-09 11:48:11 -0500

    Update DefenderTackleZone.cs

--- Assets/Scripts/NetworkGameplay/DefenderTackleZone.cs
+++ Assets/Scripts/NetworkGameplay/DefenderTackleZone.cs
@@ -1,1 +1,25 @@
 using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class DefenderTackleZone : MonoBehaviour
+{
+    private DefenderController mainController;
+
+    private void Awake()
+    {
+        mainController = GetComponentInParent<DefenderController>();
+        Collider col = GetComponent<Collider>();
+        if (col != null)
+        {
+            col.isTrigger = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && mainController != null)
+        {
+            mainController.OnPlayerDetected(other.transform);
+        }
+    }
+}
